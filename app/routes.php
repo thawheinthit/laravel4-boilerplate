c8843ff0a47a3b8a83122fc1c8cca167e4837c34
thawheinthit <thawheinthit@gmail.com>
2014-07-27 15:22:33 +0630

    Initialized the project

---
+++ app/routes.php
@@ -0,0 +1,36 @@
+<?php
+/*
+|--------------------------------------------------------------------------
+| WebService Resource Routes 
+|--------------------------------------------------------------------------
+*/
+
+Route::group(array('prefix' => 'api'), function() {
+    Route::resource('users', 'Controllers\\API\\UsersController');
+    // Route::resource('posts', 'Controllers\\API\\PostController');
+});
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes // Can't access without authentication
+|--------------------------------------------------------------------------
+*/
+
+Route::group(array('before' => 'auth'), function() {
+    Route::controllers(
+        array(
+            'dashboard' => 'DashboardController',
+            'post' => 'PostController'
+        )
+    );
+});
+
+
+Route::get('/', function()
+{
+	return View::make('home.index');
+});
+
+Route::controllers( array('user' => 'UserController'));
